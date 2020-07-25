07174f092d19dd5bfd095c2b91e325af8f4ec5aa
Ajit Dubey <ajitdubey.it@gmail.com>
2020-07-25 23:38:36 +0530

    created OAuth demo using facebook

---
+++ OAuth-facebookLogin/src/test/java/com/example/OAuthfacebookLogin/OAuthFacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OAuthfacebookLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthFacebookLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
